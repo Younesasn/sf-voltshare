7bde4981adc23bcd8154ce9ce99e3eb9a65a2410
ASSANI Younes <younesassani@gmail.com>
2025-04-14 23:58:25 +0200

    création des fixtures User/Car

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Car;
+use App\Entity\Timeslot;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        $user = new User();
+        $user->setFirstname($faker->firstName)
+            ->setLastname($faker->lastName)
+            ->setEmail($faker->email)
+            ->setPassword('test')
+            ->setAdress($faker->address)
+            ->setTel($faker->phoneNumber);
+        $manager->persist($user);
+
+        $car = new Car();
+        $car->setModel('Tesla Model 3')
+            ->setUser($user);
+        $manager->persist($car);
 
         $manager->flush();
     }
