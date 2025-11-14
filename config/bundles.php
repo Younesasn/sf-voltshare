a95334d8be21d4830ea4aefa5fd600d09a3899ac
ASSANI Younes <younesassani@gmail.com>
2025-11-14 13:48:14 +0100

    correction bug bundle dev

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
@@ -9,9 +9,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     CoopTilleuls\ForgotPasswordBundle\CoopTilleulsForgotPasswordBundle::class => ['all' => true],
@@ -19,6 +16,26 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pontedilana\WeasyprintBundle\WeasyprintBundle::class => ['all' => true],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
+
+if (class_exists(Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class)) {
+    $bundles[Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class] = ['dev' => true, 'test' => true];
+}
+
+if (class_exists(Symfony\Bundle\MakerBundle\MakerBundle::class)) {
+    $bundles[Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+if (class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
+    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'test' => true];
+}
+
+if (class_exists(Zenstruck\Foundry\ZenstruckFoundryBundle::class)) {
+    $bundles[Zenstruck\Foundry\ZenstruckFoundryBundle::class] = ['dev' => true, 'test' => true];
+}
+
+if (class_exists(DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class)) {
+    $bundles[DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class] = ['test' => true];
+}
+
+return $bundles;
